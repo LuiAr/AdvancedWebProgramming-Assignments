e59f411d6f0707969012a5d6c70ab6c91dfacc61
Louis Arbey <louis.arbey@efrei.net>
2022-04-07 18:09:50 -0700

    added function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Test succesfull  !!!! Yey")
+
+const project_name = "The new r/place";
+const authors_names = "Theo MARTIN Louis ARBEY Matteo CLABASSI";
+
+function informations(project_name , authors_name) {
+    console.log("The name of the project : " + project_name + "\nAuthors : " + authors_name);
+}
